435c96e0727ee8eb06525ff30d60898364937f3d
rohit <rohit.badekar@cadienttalent.com>
2025-06-25 16:21:29 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h1 className="flex justify-center items-center underline text-4xl">Welcome</h1>
+      <Button>Try Me</Button>
+    </>
+  );
+};
+
+export default Page;
